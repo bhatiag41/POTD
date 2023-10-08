60d205ac8f94e919c8bd9d987de4e834efe004e7
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2023-10-08 15:25:51 +0530

    Create POTD2.3Q1.java

---
+++ POTD2.3Q1.java
@@ -0,0 +1,35 @@
+// Java Program to print pattern 
+// Mirror Image of a triangle 
+import java.util.*; 
+
+public class Main { 
+	// Function to demonstrate pattern 
+	public static void printPattern(int n) 
+	{ 
+		int i, j; 
+		for (i = 1; i <= n; i++) { 
+			for (j = 1; j < i; j++) { 
+				System.out.print(" "); 
+			} 
+			for (j = i; j <= n; j++) { 
+				System.out.print(j + " "); 
+			} 
+			System.out.println(); 
+		} 
+
+		for (i = n - 1; i >= 1; i--) { 
+			for (j = 1; j < i; j++) { 
+				System.out.print(" "); 
+			} 
+			for (j = i; j <= n; j++) { 
+				System.out.print(j + " "); 
+			} 
+			System.out.println(); 
+		} 
+	} 
+	public static void main(String args[]) 
+	{ 
+		int n = 6; 
+		printPattern(n); 
+	} 
+}
